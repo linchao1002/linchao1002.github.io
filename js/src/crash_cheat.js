37add8931323913dfcc71bdcbd7db6da64d3d562
linchao1002 <2313508047@qq.com>
2019-01-25 14:12:59 +0800

    Site updated: 2019-01-25 14:12:59

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 25 2019 14:03:08 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 25 2019 14:12:55 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 客官别走 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 欢迎大人~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
