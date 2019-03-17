96e1af2d6f1bd98acb55f00ba64ef04bf2f718c4
linchao1002 <2313508047@qq.com>
2019-03-17 15:08:17 +0800

    Site updated: 2019-03-17 15:08:16

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 17 2019 15:07:05 GMT+0800 (GMT+08:00)
+// build time:Sun Mar 17 2019 15:08:15 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon.ico");document.title="╭(°A°`)╮ 客官别走 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon.ico");document.title="(ฅ>ω<*ฅ) 欢迎大人~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
